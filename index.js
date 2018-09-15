5e2a14e7fdbfbdc67220f3e51910b1dc8566efa2
Crash83k <matt@iitnow.com>
2018-09-14 17:42:14 -0700

    Added tests (Mocha / Chai) Removed old test file.

--- index.js
+++ index.js
@@ -11,14 +11,38 @@ class Events extends Event {
 // Consolidated location for all loggers neutral of namespace.
 const allLoggers = new Events();
 
-const logLevel = process.env.LOG_LEVEL || 1;
-
 class Logger {
+  /**
+   * @returns {{debug: number, info: number, success: number, warn: number, error: number, disable: number}}
+   */
+  static get levels() {
+    return {
+      debug : 5,
+      info : 4,
+      success : 3,
+      warn : 2,
+      error : 1,
+      disable : 0,
+    }
+  };
 
-  constructor(namespace) {
+  /**
+   *
+   * @param {String} namespace (Required)
+   * @param {Number|String} [logLevel] Set the log level.
+   * @returns {{debug: <Function>, info: <Function>,
+   *          success: <Function>, warn: <Function>,
+   *          error: <Function>, events: Events,
+   *          level: <Function>}}
+   */
+  constructor(namespace, logLevel = 1) {
+    if (typeof namespace !== 'string' || namespace.trim() === '')
+      throw new Error('"Namespace" parameter cannot be empty.');
+
     this.namespace = namespace;
     this.events = new Events();
-    // return this;
+    this.logLevel = this.updateDebugLevel(logLevel);
+
     return {
       debug : this.debug.bind(this),
       info : this.info.bind(this),
@@ -26,71 +50,107 @@ class Logger {
       warn : this.warn.bind(this),
       error : this.error.bind(this),
       events : this.events,
+      level : this.updateDebugLevel.bind(this),
     };
   }
 
+  /**
+   * Updates the log level for the logger namespace.
+   * @param {String|Number} level
+   * @returns {Number}
+   */
+  updateDebugLevel(level) {
+    if (typeof level !== 'undefined') {
+      if (typeof level === 'string') {
+        level = level.toLowerCase();
+        let debugLevels = Logger.levels;
+        if (! debugLevels.hasOwnProperty(level)) {
+          throw new Error(`'${level}' is not a valid logging level.`);
+        }
+        this.logLevel = debugLevels[ level ];
+      } else if (typeof level === 'number') {
+        if (level < 0) level = 0;
+        if (level > 5) level = 5;
+        this.logLevel = level;
+      } else {
+        throw new Error('Parameter "level" must be a string or number.');
+      }
+    }
+    return this.logLevel;
+  }
+
+  /**
+   * @param {Number|String|Object|Array} [arguments]
+   */
   debug() {
-    if (logLevel >= 5) {
+    if (this.logLevel >= 5) {
       log(
         chalk.cyan('☼'),
         chalk.underline(this.namespace + ':'),
         chalk.cyan(...stringArgs(arguments))
       );
-      this.events.emit('Debug', arguments);
-      allLoggers.emit('Debug', arguments);
+      this.events.emit('Debug', ...arguments);
+      allLoggers.emit('Debug', ...arguments);
     }
   }
 
+  /**
+   * @param {Number|String|Object|Array} [arguments]
+   */
   info() {
-    if (logLevel >= 4) {
+    if (this.logLevel >= 4) {
       log(
         symbols.info,
         chalk.underline(this.namespace + ':'),
         chalk.blue(...stringArgs(arguments))
       );
-      this.events.emit('Info', arguments);
-      allLoggers.emit('Info', arguments);
+      this.events.emit('Info', ...arguments);
+      allLoggers.emit('Info', ...arguments);
     }
   }
 
+  /**
+   * @param {Number|String|Object|Array} [arguments]
+   */
   success() {
-    if (logLevel >= 3) {
+    if (this.logLevel >= 3) {
       log(
         symbols.success,
         chalk.underline(this.namespace + ':'),
         chalk.green(...stringArgs(arguments))
       );
-      this.events.emit('Success', arguments);
-      allLoggers.emit('Success', arguments);
+      this.events.emit('Success', ...arguments);
+      allLoggers.emit('Success', ...arguments);
     }
   }
 
+  /**
+   * @param {Number|String|Object|Array} [arguments]
+   */
   warn() {
-    if (logLevel >= 2) {
+    if (this.logLevel >= 2) {
       log(
         symbols.warning,
         chalk.underline(this.namespace + ':'),
         chalk.yellow(...stringArgs(arguments))
       );
-      this.events.emit('Warn', arguments);
-      allLoggers.emit('Warn', arguments);
+      this.events.emit('Warn', ...arguments);
+      allLoggers.emit('Warn', ...arguments);
     }
   }
 
+  /**
+   * @param {Number|String|Object|Array} [arguments]
+   */
   error() {
-    if (logLevel >= 1) {
+    if (this.logLevel >= 1) {
       log.error(
         symbols.error,
         chalk.underline(this.namespace + ':'),
         chalk.red(...stringArgs(arguments))
       );
-
-      try {
-        this.events.emit('Error', arguments);
-        allLoggers.emit('Error', arguments);
-      } catch (e) {
-        console.error(e);
-      }
+      this.events.emit('Error', ...arguments);
+      allLoggers.emit('Error', ...arguments);
     }
   }
 }
@@ -111,6 +171,6 @@ function stringArgs(args) {
   return args;
 }
 
-exports.allEvents = allLoggers;
 module.exports = Logger;
+module.exports.allEvents = allLoggers;
 global.Logger = Logger;
---
+++ tests/level.test.js
@@ -0,0 +1,102 @@
+const Logger = require('../index');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Test debug levels being set.', () => {
+  const log0 = new Logger('Test 0');
+  it('Accepts numeric levels', () => {
+    const levelsNum = [ 5, 4, 3, 2, 1, 0 ];
+
+    for (let level of levelsNum) {
+      expect(log0.level(level)).to.equal(level);
+    }
+  });
+
+  it('Accepts string levels', () => {
+
+    [ [ 'debug', 5 ],
+      [ 'info', 4 ],
+      [ 'success', 3 ],
+      [ 'warn', 2 ],
+      [ 'error', 1 ],
+      [ 'disable', 0 ] ].forEach(level => {
+      expect(log0.level(level[ 0 ])).to.equal(level[ 1 ]);
+    });
+
+  });
+
+  it(`Doesn't accept string levels that don't exist.`, () => {
+    expect(() => log0.level('NotALevel'))
+      .to.throw(Error)
+      .with.property('message', `'notalevel' is not a valid logging level.`);
+  });
+
+  it(`Resets numeric levels outside of 0-5.`, () => {
+    expect(log0.level(- 1)).to.equal(0);
+    expect(log0.level(6)).to.equal(5);
+  });
+});
+
+describe('Test namespace logging subscriptions.', () => {
+  const log1 = new Logger('Test 1', 5);
+  const levels = [ 'Debug', 'Info', 'Success', 'Warn', 'Error' ];
+
+  for (let level of levels) {
+    it(`Subscribes to ${level} events.`, (done) => {
+      const num = Math.random();
+      log1.events.on(level, (message, number) => {
+        expect(message).to.equal(level);
+        expect(number).to.equal(num);
+        done();
+      });
+
+      log1[ level.toLowerCase() ](level, num);
+    });
+  }
+});
+
+describe('Test global logging subscriptions.', () => {
+  const log2 = new Logger('Test 2', 5);
+  const log3 = new Logger('Test 3', 5);
+  const allEvents = Logger.allEvents;
+  const levels = [ 'Debug', 'Info', 'Success', 'Warn', 'Error' ];
+
+  for (let level of levels) {
+    it(`Subscribes to global ${level} events.`, (done) => {
+      const num = Math.random();
+      let events = 0;
+      allEvents.on(level, (message, number) => {
+        expect(message).to.equal(level);
+        expect(number).to.equal(num);
+        if (++ events === 2) complete();
+      });
+      log2[ level.toLowerCase() ](level, num);
+      log3[ level.toLowerCase() ](level, num);
+
+      function complete() {
+        // Clean up the subscription.
+        allEvents.removeAllListeners(level);
+        done()
+      }
+    });
+  }
+});
+
+describe('Test that logging ignores overridden context', () => {
+  const log4 = new Logger('Test 4', 5);
+
+  [ 'debug',
+    'info',
+    'success',
+    'warn',
+    'error',
+  ].forEach(level => {
+    it(`Rebound ${level} should not throw error.`, (done) => {
+      const log5 = log4[ level ].bind({});
+      // If context wasn't ignored, this would throw an error.
+      expect(log5(`${level} throw test.`)).to.equal(undefined);
+      done();
+    })
+  });
+
+});
